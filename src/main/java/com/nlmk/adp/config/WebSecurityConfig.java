9ebba1fc09c8a448d8598a6a7f3c905db095e016
temnikov_do <temnikov_do@nlmk.com>
2023-11-22 23:58:18 +0300

    skip security base path liquibase skip existed table

--- src/main/java/com/nlmk/adp/config/WebSecurityConfig.java
+++ src/main/java/com/nlmk/adp/config/WebSecurityConfig.java
@@ -5,14 +5,27 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * Конфигурация для PreAuthorize.
+ * Конфигурация для Spring Security.
  */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true,
         securedEnabled = true)
 @ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 @ConditionalOnProperty(value = "keycloak.enabled", havingValue = "true")
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .and()
+                .formLogin().permitAll()
+                ;
+    }
+
 }
