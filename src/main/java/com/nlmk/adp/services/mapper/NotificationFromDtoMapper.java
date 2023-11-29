7ea4376e1e0be2636d5a3f6e995cb5476ecc0e13
Носенко Константин Сергеевич (nosenko_ks) <nosenko_ks@nlmk.com>
2023-11-30 02:57:35 +0400

    hotfix

--- src/main/java/com/nlmk/adp/services/mapper/NotificationFromDtoMapper.java
+++ src/main/java/com/nlmk/adp/services/mapper/NotificationFromDtoMapper.java
@@ -1,6 +1,7 @@
 package com.nlmk.adp.services.mapper;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.mapstruct.Mapper;
@@ -35,12 +36,46 @@ public interface NotificationFromDtoMapper {
              expression = "java(mapRolesFromDto(notificationDto, com.nlmk.adp.services.mapper.RoleStatus.REJECT))")
     @Mapping(target = "data.acceptEmails",
              expression = "java(mapEmailsFromDto(notificationDto))")
+    @Mapping(target = "data.type", constant = "INFO")
     @Mapping(target = "metadata.kafkaTimestamp", source = "kafkaDt")
+    @Mapping(target = "metadata.kafkaKey", source = "uuid")
+    @Mapping(target = "metadata.kafkaTopic", source = "uuid")
     @Mapping(target = "ts", source = "kafkaDt")
     @Mapping(target = "op", constant = "I")
-    @Mapping(target = "pk", ignore = true)
-    @Mapping(target = "sys", ignore = true)
+    @Mapping(target = "pk", source = "uuid")
+    @Mapping(target = "sys", source = "uuid")
     DbUserNotificationVer0 mapDataFromDto(NotificationDto notificationDto);
+
+    /**
+     * 123.
+     *
+     * @param id
+     *         123.
+     *
+     * @return 123.
+     */
+    default nlmk.l3.mesadp.db.user.notification.ver0.PkType mapToPk(UUID id) {
+        return nlmk.l3.mesadp.db.user.notification.ver0.PkType
+                .newBuilder()
+                .setId(id.toString())
+                .build();
+    }
+
+    /**
+     * 123.
+     *
+     * @param id
+     *         123.
+     *
+     * @return 123.
+     */
+    default nlmk.Sys maptoSys(UUID id) {
+        return nlmk.Sys
+                .newBuilder()
+                .setSeqID(1)
+                .setTraceID(UUID.randomUUID().toString())
+                .build();
+    }
 
     /**
      * mapRolesFromDto.
